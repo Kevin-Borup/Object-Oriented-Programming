d73028b1e0cbee9b68399d467437241f5493cdc1
Kevin Borup - Station <kevi1291@zbc.dk>
2021-09-09 18:30:19 +0200

    VendingMachineStart

---
+++ OOP/VendingMachine/Ressource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class Ressource
+    {
+        private float money;
+        private Item[,] inventory = new Item[5, 3];
+
+        public float Money { get { return money; } private set { this.money = value; } }
+        public Item[,] Inventory { get { return inventory; } private set { this.inventory = value; } }
+    }
+
+    class Item
+    {
+        private string name;
+        private float price;
+        private int amount;
+
+        public Item(string name, float price, int amount)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+        public string Name { get { return name; } private set { this.name = value; } }
+        public float Price { get { return price; } private set { this.price = value; } }
+        public int Amount { get { return amount; } private set { this.amount = value; } }
+
+    }
+}
---
+++ OOP/VendingMachine/VendingMachine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class VendingMachine
+    {
+        Operation system = new Operation();
+        Ressource contents = new Ressource();
+
+        public void PoweredOn()
+        {
+
+        }
+        public void GetInput()
+        {
+
+        }
+        public void ShowUserMenu()
+        {
+            string mainMenu =
+                "---------------------------------------------------" +
+                "|                                                 |";
+        }
+        public void StopProces()
+        {
+
+        }
+        public void GiveProduct()
+        {
+
+        }
+        public void ShowAdminMenu()
+        {
+
+        }
+        public void RefillProducts()
+        {
+
+        }
+        public void GatherIncome()
+        {
+
+        }
+        public void ChangePrice()
+        {
+
+        }
+    }
+}
