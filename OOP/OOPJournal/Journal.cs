e83d3cdea0273991cc71afac55d2bf99c4150a0f
Kevin Borup <kevi1291@zbc.dk>
2021-09-10 15:45:52 +0200

    Journal Progress

---
+++ OOP/OOPJournal/Journal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPJournal
+{
+    class Journal
+    {
+        private string name;
+        private string address;
+        private string phone;
+        private string email;
+        private uint cpr;
+        private string prefDoctor;
+        private List<JournalEntry> entries;
+        public void AddJournalEntry(string doctor, string description)
+        {
+            entries.Add(new JournalEntry(doctor, description));
+        }
+        public string Name { get { return name; } set { name = value; } }
+        public string Address { get { return address; } set { address = value; } }
+        public string Phone { get { return phone; } set { phone = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public uint Cpr { get { return cpr; } set { cpr = value; } }
+        public string PrefDoctor { get { return prefDoctor; } set { prefDoctor = value; } }
+
+    }
+    class JournalEntry
+    {
+        private DateTime timeFormat;
+        private string doctor;
+        private string description;
+        public JournalEntry(string doctor, string description)
+        {
+            timeFormat = DateTime.Now;
+            this.doctor = doctor;
+            this.description = description;
+        }
+        public DateTime TimeFormat { get { return timeFormat; } set { timeFormat = value; } }
+        public string Doctor { get { return doctor; } set { doctor = value; } }
+        public string Description { get { return description; } set { description = value; } }
+    }
+}
---
+++ OOP/OOPJournal/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPJournal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System sys = new System();
+            sys.Process();
+        }
+
+        
+    }
+    class System
+    {
+        public void Process()
+        {
+            string[] journalDetails;
+            string[] entries;
+
+
+        }
+
+        private void JournalVisual()
+        {
+            string journal = 
+                "Name               CPR\n" +
+                "Address            PrefDoctor\n" +
+                "Phone / Email      Age: year & days\n";
+            string seperation = 
+                "-------------------------------------------------\n";
+            string entry = 
+                "DateTime - Doctor\n" +
+                "   Description";
+
+            Console.WriteLine(journal + seperation + entry);
+        }
+    }
+}
